da3017245db3bdb2c3478e60b29f97f09fcc633a
Tom Vercauteren <tom.vercauteren@kcl.ac.uk>
2026-08-24 14:04:15 +0100

    Update FindASan.cmake (#23)
    * Update FindASan.cmake
    
    Addresses #20
    
    * Update FindASan.cmake
    
    GCC ASan library is often located in a compiler-specific directory
    
    * Update FindASan.cmake

--- cmake/FindASan.cmake
+++ cmake/FindASan.cmake
@@ -1,6 +1,59 @@
 option(USE_ASAN "Activate ASan compiler/linker options" OFF)
 
 if(USE_ASAN)
+  find_library(
+    ASAN_LIBRARY
+    NAMES
+      asan
+      libasan
+      libclang_rt.asan-x86_64
+      libclang_rt.asan-aarch64
+  )
+
+  if(NOT ASAN_LIBRARY)
+    message(STATUS
+      "find_library() could not locate an ASan runtime. "
+      "Falling back to compiler-provided location."
+    )
+
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+      execute_process(
+        COMMAND ${CMAKE_CXX_COMPILER} -print-file-name=libasan.so
+        OUTPUT_VARIABLE ASAN_LIBRARY
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+
+      # GCC returns the argument unchanged on failure.
+      if(ASAN_LIBRARY STREQUAL "libasan.so")
+        unset(ASAN_LIBRARY)
+      endif()
+
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+      execute_process(
+        COMMAND ${CMAKE_CXX_COMPILER} -print-runtime-dir
+        OUTPUT_VARIABLE CLANG_RUNTIME_DIR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+      )
+
+      find_library(
+        ASAN_LIBRARY
+        NAMES
+          libclang_rt.asan-x86_64
+          libclang_rt.asan-aarch64
+        PATHS ${CLANG_RUNTIME_DIR}
+        NO_DEFAULT_PATH
+      )
+    endif()
+  endif()
+
+  if(NOT ASAN_LIBRARY)
+    message(FATAL_ERROR
+      "Unable to locate the AddressSanitizer runtime."
+    )
+  endif()
+
+  message(STATUS "Using ASAN_LIBRARY: ${ASAN_LIBRARY}")
+  
   if(MSVC)
     add_compile_options(/fsanitize=address)
     add_link_options(/fsanitize=address)
@@ -14,35 +67,10 @@ if(USE_ASAN)
 
   set(ASan_FOUND TRUE)
 
-  if(NOT MSVC AND NOT APPLE)
-    find_library(ASAN_LIBRARY NAMES asan libasan)
-    if(NOT ASAN_LIBRARY)
-      execute_process(
-        COMMAND ${CMAKE_CXX_COMPILER} -print-file-name=libasan.so
-        OUTPUT_VARIABLE ASAN_DRIVER_PATH
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-      if(EXISTS "${ASAN_DRIVER_PATH}")
-        set(ASAN_LIBRARY "${ASAN_DRIVER_PATH}")
-      endif()
-    endif()
-
-    find_library(STDCXX_LIBRARY NAMES stdc++ libstdc++.so.6)
-    if(NOT STDCXX_LIBRARY)
-      execute_process(
-        COMMAND ${CMAKE_CXX_COMPILER} -print-file-name=libstdc++.so.6
-        OUTPUT_VARIABLE STDCXX_DRIVER_PATH
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-      if(EXISTS "${STDCXX_DRIVER_PATH}")
-        set(STDCXX_LIBRARY "${STDCXX_DRIVER_PATH}")
-      endif()
-    endif()
-
+  if(NOT MSVC)
     set(PRELOAD_LIBS "")
     if(ASAN_LIBRARY)
       list(APPEND PRELOAD_LIBS "${ASAN_LIBRARY}")
-    endif()
-    if(STDCXX_LIBRARY)
-      list(APPEND PRELOAD_LIBS "${STDCXX_LIBRARY}")
     endif()
 
     if(PRELOAD_LIBS)
